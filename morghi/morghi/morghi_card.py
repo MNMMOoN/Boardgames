13423372b32d4713881166199fcd48e1f61dcdf3
Mohammad Nourbakhsh Marvast <m.nourbakhsh2000@gmail.com>
2025-12-12 11:32:43 +0330

    add is_animal to morghi_card.py instead of kind

--- morghi/morghi/morghi_card.py
+++ morghi/morghi/morghi_card.py
@@ -1,5 +1,5 @@
 class Card:
-    def __init__(self, name, kind, max_plays=None):
+    def __init__(self, name: str, is_animal: bool | None, max_plays: int = 0):
         """
         Parameters:
             name (str)        – name of the card
@@ -7,30 +7,17 @@ class Card:
             max_plays (int)   – how many times this card can be used
         """
         self.name = name
-        self.kind = kind
+        self.is_animal = None
         self.max_plays = max_plays
         self.times_played = 0
-        self.playable = True
+        self._playable = True
 
     def can_play(self):
         """Check if the card can still be played."""
         if self.max_plays is None:
-            return self.playable
+            return self._playable
         
         if self.times_played > self.max_plays:
-            self.playable = False
+            self._playable = False
 
-        return self.playable
-
-    def play(self, target=None):
-        """General play method (to be overridden by child classes)."""
-        if not self.can_play():
-            print(f"{self.name} cannot be played anymore.")
-            return
-
-        self.times_played += 1
-        if self.max_plays is None:
-            print(f"{self.name} was played! (used {self.times_played})")
-        else:
-            print(f"{self.name} was played! (used {self.times_played}/{self.max_plays})")
-        # Child classes override this to implement functionality
+        return self._playable
