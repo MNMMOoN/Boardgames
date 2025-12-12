70d59ae9ed17cc1e3b338910199492bb0b9bb5e3
Mohammad Nourbakhsh Marvast <m.nourbakhsh2000@gmail.com>
2025-12-12 11:34:16 +0330

    Remove unnecessary .py files and classes

--- morghi/morghi/morghi_nonanimal.py
+++
@@ -1,6 +0,0 @@
-from morghi_card import Card
-
-class NonAnimal(Card):
-    def __init__(self, name, kind, max_plays=None):
-        # Correct call to parent constructor
-        super().__init__(name, kind, max_plays)
